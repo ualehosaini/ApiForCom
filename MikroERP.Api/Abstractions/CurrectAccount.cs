076e485aa8b3a1661afbdc1a7c384db7b511908e
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-03-24 22:38:00 +0000

    CurrectAccount added

---
+++ MikroERP.Api/Abstractions/CurrectAccount.cs
@@ -0,0 +1,18 @@
+﻿namespace MikroERP.Api.Abstractions
+{
+    public class CurrectAccount
+    {
+        public int MyProperty { get; set; }
+        public string cari_kod { get; set; }
+        public string cari_unvan1 { get; set; }
+        public string cari_vdaire_adi { get; set; }
+        public string cari_vdaire_no { get; set; }
+        public string cari_fatura_adres_no { get; set; }
+        public string cari_sevk_adres_no { get; set; }
+        public string cari_wwwadresi { get; set; }
+        public string cari_EMail { get; set; }
+        public string cari_banka_swiftkodu10 { get; set; }
+        public string cari_CepTel { get; set; }
+        public string cari_muh_kod { get; set; }
+    }
+}
